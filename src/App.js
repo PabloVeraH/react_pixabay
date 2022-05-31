6686431c4fbc92d904fd9ac89623b12b620e5f4a
Pablo Vera <pablo.vera82@gmail.com>
2022-05-30 22:53:31 -0400

    Add function to call the images API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 
 function App() {
-  console.log(process.env.React_App_Title);
+
+  // state de la app
+  const [ busqueda, guardarBusqueda ] = useState('');
+
+  useEffect(() => {
+    const consultarApi = async () => {
+        if(busqueda === '' ) return;
+        console.log('busqueda: '+busqueda);
+
+        const imagenesPorPagina = process.env.React_App_Images_Per_Page;
+        const key = process.env.React_App_Pixabay_Key;
+        const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+        console.log('url: '+url);
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        console.log(resultado.hits);
+
+        guardarBusqueda(resultado.hits);
+    }
+    consultarApi();
+  }, [busqueda])
+
 
   return (
   <div className="container">
     <div className="jumbotron">
       <p className="lead text-center">Image Finder</p>
 
-      <Formulario />
+      <Formulario
+        guardarBusqueda={guardarBusqueda}
+      />
     </div>
   </div>
   );
